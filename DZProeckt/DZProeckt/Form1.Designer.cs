450b78c359f390a43e4b26091157fef69e50be2b
Maxim707 <76256465+Maxim707@users.noreply.github.com>
2021-01-14 16:29:42 +0300

    Add files via upload

---
+++ DZProeckt/DZProeckt/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace DZProeckt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPasword = new System.Windows.Forms.TextBox();
+            this.labelPasword = new System.Windows.Forms.Label();
+            this.buttonAnswer = new System.Windows.Forms.Button();
+            this.textBoxSymbol = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxPasword
+            // 
+            this.textBoxPasword.Location = new System.Drawing.Point(24, 67);
+            this.textBoxPasword.Name = "textBoxPasword";
+            this.textBoxPasword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPasword.TabIndex = 0;
+            // 
+            // labelPasword
+            // 
+            this.labelPasword.AutoSize = true;
+            this.labelPasword.Location = new System.Drawing.Point(32, 25);
+            this.labelPasword.Name = "labelPasword";
+            this.labelPasword.Size = new System.Drawing.Size(82, 13);
+            this.labelPasword.TabIndex = 1;
+            this.labelPasword.Text = "Новый Пороль";
+            // 
+            // buttonAnswer
+            // 
+            this.buttonAnswer.Location = new System.Drawing.Point(35, 93);
+            this.buttonAnswer.Name = "buttonAnswer";
+            this.buttonAnswer.Size = new System.Drawing.Size(75, 47);
+            this.buttonAnswer.TabIndex = 2;
+            this.buttonAnswer.Text = "Подобрать пороль";
+            this.buttonAnswer.UseVisualStyleBackColor = true;
+            this.buttonAnswer.Click += new System.EventHandler(this.buttonAnswer_Click);
+            // 
+            // textBoxSymbol
+            // 
+            this.textBoxSymbol.Location = new System.Drawing.Point(24, 41);
+            this.textBoxSymbol.Name = "textBoxSymbol";
+            this.textBoxSymbol.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSymbol.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 166);
+            this.Controls.Add(this.textBoxSymbol);
+            this.Controls.Add(this.buttonAnswer);
+            this.Controls.Add(this.labelPasword);
+            this.Controls.Add(this.textBoxPasword);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPasword;
+        private System.Windows.Forms.Label labelPasword;
+        private System.Windows.Forms.Button buttonAnswer;
+        private System.Windows.Forms.TextBox textBoxSymbol;
+    }
+}
+
---
+++ DZProeckt/DZProeckt/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZProeckt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random1;
+        Random random2;
+        string number1;
+        int number;
+        int numberLetter;
+
+        private void buttonAnswer_Click(object sender, EventArgs e)
+        {
+            random1 = new Random();
+            random2 = new Random();
+            number = random1.Next(1, 9 + 1);
+            numberLetter = random2.Next(1, 26 + 1);
+
+            switch (numberLetter)
+            {
+                case 1:
+                    number1 = "A";
+                    break;
+
+                case 2:
+                    number1 = "b";
+                    break;
+
+                case 3:
+                    number1 = "c";
+                    break;
+
+                case 4:
+                    number1 = "D";
+                    break;
+
+                case 5:
+                    number1 = "e";
+                    break;
+
+                case 6:
+                    number1 = "F";
+                    break;
+
+                case 7:
+                    number1 = "G";
+                    break;
+
+                case 8:
+                    number1 = "H";
+                    break;
+
+                case 9:
+                    number1 = "i";
+                    break;
+
+                case 10:
+                    number1 = "J";
+                    break;
+
+                case 11:
+                    number1 = "K";
+                    break;
+
+                case 12:
+                    number1 = "l";
+                    break;
+
+                case 13:
+                    number1 = "m";
+                    break;
+
+                case 14:
+                    number1 = "N";
+                    break;
+
+                case 15:
+                    number1 = "O";
+                    break;
+
+                case 16:
+                    number1 = "P";
+                    break;
+
+                case 17:
+                    number1 = "q";
+                    break;
+
+                case 18:
+                    number1 = "R";
+                    break;
+
+                case 19:
+                    number1 = "s";
+                    break;
+
+                case 20:
+                    number1 = "T";
+                    break;
+
+                case 21:
+                    number1 = "U";
+                    break;
+
+                case 22:
+                    number1 = "V";
+                    break;
+
+                case 23:
+                    number1 = "w";
+                    break;
+
+                case 24:
+                    number1 = "X";
+                    break;
+
+                case 25:
+                    number1 = "y";
+                    break;
+
+                case 26:
+                    number1 = "Z";
+                    break;
+            }
+
+        }
+    }
+}
